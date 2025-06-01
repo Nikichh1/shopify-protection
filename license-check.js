2bd186a1ca614980afcee128b5b245940b0d5469
Nikichh1 <chavdarov08@gmail.com>
2025-06-01 14:38:26 +0300

    Create license-check.js

---
+++ license-check.js
@@ -0,0 +1,12 @@
+fetch("https://yourname.github.io/shopify-protection/license.json")
+  .then(res => res.json())
+  .then(data => {
+    if (!data.valid || data.key !== "robotnik-2025") {
+      document.body.innerHTML = "";
+      console.warn("Invalid license.");
+    }
+  })
+  .catch(() => {
+    document.body.innerHTML = "";
+    console.error("License check failed.");
+  });
