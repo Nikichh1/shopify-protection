4e8f0485e5ee0ad9b8a29a7aefba61a03e7bbf8a
Nikichh1 <chavdarov08@gmail.com>
2025-06-01 15:48:47 +0300

    Update license-check.js

--- license-check.js
+++ license-check.js
@@ -1,12 +1,10 @@
-fetch("https://nikichh1.github.io/shopify-protection/license.js")
-  .then(res => res.json())
-  .then(data => {
-    if (!data.valid || data.key !== "robotnik-2025") {
-      document.body.innerHTML = "";
-      console.warn("Invalid license.");
-    }
-  })
-  .catch(() => {
+function licenseCallback(data) {
+  if (!data.valid || data.key !== "robotnik-2025") {
     document.body.innerHTML = "";
-    console.error("License check failed.");
-  });
+    console.warn("Invalid license.");
+  }
+}
+
+var script = document.createElement('script');
+script.src = "https://nikichh1.github.io/shopify-protection/license.js";
+document.head.appendChild(script);
