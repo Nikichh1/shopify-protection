36b9974868d2eae02644fc8b77958ceb125961f3
Nikichh1 <chavdarov08@gmail.com>
2026-03-31 17:39:01 +0300

    Refactor license validation in license-check-ps.js
    Simplified license validation logic and removed overlay display.

--- license-check-ps.js
+++ license-check-ps.js
@@ -1,28 +1,7 @@
 function licenseCallback(data) {
-  if (!data.valid || data.key !== "robotnik-2025") {
-
-    const overlay = document.createElement("div");
-    overlay.style.position = "fixed";
-    overlay.style.top = "0";
-    overlay.style.left = "0";
-    overlay.style.width = "100%";
-    overlay.style.height = "100%";
-    overlay.style.background = "#111";
-    overlay.style.color = "#fff";
-    overlay.style.display = "flex";
-    overlay.style.alignItems = "center";
-    overlay.style.justifyContent = "center";
-    overlay.style.zIndex = "999999";
-    overlay.style.fontFamily = "Arial, sans-serif";
-
-    overlay.innerHTML = `
-    `;
-
-    document.body.style.overflow = "hidden";
-
-    document.body.appendChild(overlay);
-
-    console.warn("Invalid license.");
+  if (!data.valid || data.key !== "robotnik-202") {
+    document.body.innerHTML = "";
+    console.warn("Error");
   }
 }
 
